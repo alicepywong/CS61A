1b09da15c6225e4db2f6ae6bdff464fcbf7e8a45
alicepywong <alicepywong715@gmail.com>
2022-08-06 11:33:44 +0800

    disc03

---
+++ discussion/disc03_recursion.py
@@ -0,0 +1,127 @@
+# Q1: Warm Up: Recursive Multiplication
+def multiply(m, n):
+    """ Takes two positive integers and returns their product using recursion.
+    >>> multiply(5, 3)
+    15
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return m
+    else:
+        return m + multiply(m, n - 1)
+
+
+# Q4: Is Prime
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def func(m):
+        if m == 1:
+            return True
+        elif n % m == 0:
+            return False
+        else:
+            return func(m-1)
+
+    return func(n-1)
+
+
+# Q5: Recursive Hailstone
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    print(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 1:
+        return 1 + hailstone(3 * n + 1)
+    else:
+        return 1 + hailstone(n // 2)
+
+
+# Q6: Merge Numbers
+def merge(n1, n2):
+    """ Merges two numbers by digit in decreasing order
+    >>> merge(31, 42)
+    4321
+    >>> merge(21, 0)
+    21
+    >>> merge (21, 31) 
+    3211
+    """
+    "*** YOUR CODE HERE ***"
+    print(n1, n2)
+    if n1 == 0:
+        return n2
+    elif n2 == 0:
+        return n1
+    elif n1 % 10 < n2 % 10:
+        return merge(n1 // 10, n2) * 10 + n1 % 10
+    else:
+        return merge(n1, n2 // 10) * 10 + n2 % 10
+
+
+# Q7: Count Stair Ways
+def count_stair_ways(n):
+    """Returns the number of ways to climb up a flight of
+    n stairs, moving either 1 step or 2 steps at a time.
+    >>> count_stair_ways(4)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return count_stair_ways(n-1) + count_stair_ways(n-2)
+
+
+# Q8: Count K
+def count_k(n, k):
+    """ Counts the number of paths up a flight of n stairs
+    when taking up to and including k steps at a time.
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        total = 0
+        i = 1
+        while i <= k:
+            total += count_k(n - i, k)
+            i += 1
+        return total
