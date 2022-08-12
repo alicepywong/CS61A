321114967d2041c020140f286549529502c56e85
alicepywong <alicepywong715@gmail.com>
2022-08-12 22:42:55 +0800

    disc07

---
+++ discussion/disc07_trees&linked_lists.py
@@ -0,0 +1,57 @@
+#########
+# Trees #
+#########
+class Tree:
+    def __init__(self, label, branches=[]):
+        self.label = label
+        self.branches = branches
+
+    def is_leaf(self):
+        return self.branches == []
+
+
+# Q1: Height
+def height(t):
+    """Return the height of a tree.
+
+    >>> t = Tree(3, [Tree(5, [Tree(1)]), Tree(2)])
+    >>> height(t)
+    2
+    >>> t = Tree(3, [Tree(1), Tree(2, [Tree(5, [Tree(6)]), Tree(1)])])
+    >>> height(t)
+    3
+    """
+    if t.is_leaf():
+        return 0
+    return 1 + max([height(b)] for b in t.branches)
+
+
+# Q2: Maximum Path Sum
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+
+    >>> t = Tree(1, [Tree(5, [Tree(1), Tree(3)]), Tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    if t.is_leaf():
+        return t.label
+    else:
+        return t.label + max([max_path_sum(b) for b in t.branches])
+
+
+# Q3: Find Path
+def find_path(t, x):
+    """
+    >>> t = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if t.label == x:
+        return [t.label]
+    for b in t.branches:
+        path = find_path(b, x)
+        if path:
+            return [t.label] + path
